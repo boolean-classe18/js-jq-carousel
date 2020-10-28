060fadca2286167031bc327ca30860eb1392e253
Sofia <Perlari>
2020-10-28 10:06:08 +0100

    pallini spostati assieme a img

--- main.js
+++ main.js
@@ -2,21 +2,30 @@ $(document).ready(function() {
 
     // intercetto il click sull'icona "next"
     $('.next i').click(function() {
+
         // recupero l'immagine corrente
         var img_corrente = $('img.active');
+        // recupero il pallino corrente
+        var pallino_corrente = $('span.active');
 
         // tolgo la classe active all'immagine corrente
         img_corrente.removeClass('active');
+        // tolgo la classe active al pallino corrente
+        pallino_corrente.removeClass('active');
 
         // verifico se esiste un'immagine successiva
         if(img_corrente.next('img').length) {
             // c'è un elemento successivo
             // assegno la classe active all'immagine successiva
             img_corrente.next('img').addClass('active');
+            // assegno la classe active al pallino successivo
+            pallino_corrente.next('span').addClass('active');
         } else {
-            // non c'è un elemento successivo
+            // non c'è un'immagine successiva
             // assegno la classe active alla prima immagine
-            $('.slides :first-child').addClass('active');
+            $('.slides img:first-child').addClass('active');
+            // assegno la classe active al primo pallino
+            $('.bullets span:first-child').addClass('active');
         }
     });
 
@@ -24,19 +33,27 @@ $(document).ready(function() {
     $('.prev i').click(function() {
         // recupero l'immagine corrente
         var img_corrente = $('img.active');
+        // recupero il pallino corrente
+        var pallino_corrente = $('span.active');
 
         // tolgo la classe active all'immagine corrente
         img_corrente.removeClass('active');
+        // tolgo la classe active al pallino corrente
+        pallino_corrente.removeClass('active');
 
         // verifico se esiste un'immagine precedente
         if(img_corrente.prev('img').length) {
-            // c'è un elemento precedente
+            // c'è un'immagine precedente
             // assegno la classe active all'immagine precedente
             img_corrente.prev('img').addClass('active');
+            // assegno la classe active al pallino precendete
+            pallino_corrente.prev('span').addClass('active');
         } else {
             // non c'è un elemento precedente
             // assegno la classe active all'ultima immagine
             $('.slides img:last-of-type').addClass('active');
+            // assegno la classe active all'ultimo pallino
+            $('.bullets span:last-child').addClass('active');
         }
     });
 
